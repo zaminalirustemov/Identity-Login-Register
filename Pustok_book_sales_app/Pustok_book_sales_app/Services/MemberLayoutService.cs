636127c70b0b37288e77cc71f783debd7405beb7
Zaminali <2g17rck@code.eddu.az>
2023-01-16 06:14:10 +0400

    HomeWork

---
+++ Pustok_book_sales_app/Pustok_book_sales_app/Services/MemberLayoutService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Pustok_book_sales_app.Models;
+
+namespace Pustok_book_sales_app.Services
+{
+    public class MemberLayoutService
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public MemberLayoutService(UserManager<AppUser> userManager, IHttpContextAccessor httpContextAccessor)
+        {
+            _userManager = userManager;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public async Task<AppUser> GetUser()
+        {
+            string name = _httpContextAccessor.HttpContext.User.Identity.Name;
+
+            AppUser appUser = await _userManager.FindByNameAsync(name);
+
+
+            return appUser;
+        }
+    }
+}
